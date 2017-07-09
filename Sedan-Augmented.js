abac257917499f14fedec0e0671642e9fae24091
Helana <helanan@gmail.com>
2017-07-09 17:00:53 -0500

    reformated, added new functions, new getters and setters, and finished styling. Ready to be deployed as a SPA.

--- Sedan-Augmented.js
+++ Sedan-Augmented.js
@@ -1,21 +1,132 @@
 var Sedan = (function(newSedan){
-	var max_occupancy = 6;
-	var current_occupancy = 0;
+	var max_occupancy = 8;
+	var current_occupancyOne = 4;
+	var current_occupancyTwo = 3;
+	var current_occupancyThree = 6;
+	var current_occupancyFour = 2;
+	var current_occupancyFive = 4;
+	var current_occupancySix = 4;
 
-	newSedan.getOccupancy = function(){
-		return current_occupancy;
+	newSedan.getOccupancyOne = function(){
+		return current_occupancyOne;
 	};
 
-	newSedan.setOccupancy = function(occupancy){
+	newSedan.getOccupancyTwo = function(){
+		return current_occupancyTwo;
+	};
+
+	newSedan.getOccupancyThree = function(){
+		return current_occupancyThree;
+	};
+
+	newSedan.getOccupancyFour = function(){
+		return current_occupancyFour;
+	};
+
+	newSedan.getOccupancyFive = function(){
+		return current_occupancyFive;
+	};
+
+	newSedan.getOccupancySix = function(){
+		return current_occupancySix;
+	};
+
+	newSedan.setOccupancyOne = function(occupancy){
 		if(occupancy <= max_occupancy){
-			current_occupancy = occupancy;
+			current_occupancyOne = occupancy;
 		} else {
 			throw "Cannot exceed maximum occupancy of" + max_occupancy;
 		}
 	};
-	newSedan.getType = function(){
-		return "minivan";
+
+	newSedan.setOccupancyTwo = function(occupancy){
+		if(occupancy <= max_occupancy){
+			current_occupancyTwo = occupancy;
+		} else {
+			throw "Cannot exceed maximum occupancy of" + max_occupancy;
+		}
 	};
+
+	newSedan.setOccupancyThree = function(occupancy){
+		if(occupancy <= max_occupancy){
+			current_occupancyThree = occupancy;
+		} else {
+			throw "Cannot exceed maximum occupancy of" + max_occupancy;
+		}
+	};
+
+	newSedan.setOccupancyFour = function(occupancy){
+		if(occupancy <= max_occupancy){
+			current_occupancyFour = occupancy;
+		} else {
+			throw "Cannot exceed maximum occupancy of" + max_occupancy;
+		}
+	};
+
+	newSedan.setOccupancyFive = function(occupancy){
+		if(occupancy <= max_occupancy){
+			current_occupancyFive = occupancy;
+		} else {
+			throw "Cannot exceed maximum occupancy of" + max_occupancy;
+		}
+	};
+
+	newSedan.setOccupancySix = function(occupancy){
+		if(occupancy <= max_occupancy){
+			current_occupancySix = occupancy;
+		} else {
+			throw "Cannot exceed maximum occupancy of" + max_occupancy;
+		}
+	};
+
+	newSedan.getTypeOne = function(){
+		return "Porsche Boxter";
+	};
+
+	newSedan.getTypeTwo = function(){
+		return "BMW Sedan";
+	};
+
+	newSedan.getTypeThree = function(){
+		return "Volvo Hatchback";
+	};
+
+	newSedan.getTypeFour = function(){
+		return "Acura Sedan";
+	};
+
+	newSedan.getTypeFive = function(){
+		return "Acura Sedan";
+	};
+
+	newSedan.getTypeSix = function(){
+		return "Acura Sedan";
+	};
+
+	newSedan.getColorOne = function(){
+		return "Blue";
+	};
+
+	newSedan.getColorTwo = function(){
+		return "Red";
+	};
+
+	newSedan.getColorThree = function(){
+		return "Green";
+	};
+
+	newSedan.getColorFour = function(){
+		return "Orange";
+	};
+
+	newSedan.getColorFive = function(){
+		return "Grey";
+	};
+
+	newSedan.getColorSix = function(){
+		return "Yellow";
+	};
+
 	
 	return newSedan;
 
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-console.log("Sedan IIFE", Sedan);
-console.log("Sedan Color", Sedan.getColor());
-console.log("Sedan type", Sedan.getType());
-console.log("Sedan get Occupancy", Sedan.getOccupancy());
+// console.log("Sedan IIFE", Sedan);
+// console.log("Sedan Color", Sedan.getColorOne());
+// console.log("Sedan type", Sedan.getTypeOne());
+// console.log("Sedan get Occupancy", Sedan.getOccupancyOne());
+// console.log("Sedan Manufacterur Date", Sedan.manufactured_Date);
 
+document.getElementById("typeOne").innerHTML = Sedan.getTypeOne();
+document.getElementById("occupancyOne").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancyOne();
+document.getElementById("colorOne").innerHTML = 'Color Available:' + ' ' + Sedan.getColorOne();
+document.getElementById("manufacturedOne").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
+
+document.getElementById("typeTwo").innerHTML = Sedan.getTypeTwo();
+document.getElementById("occupancyTwo").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancyTwo();
+document.getElementById("colorTwo").innerHTML = 'Color Available:' + ' ' + Sedan.getColorTwo();
+document.getElementById("manufacturedTwo").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
+
+document.getElementById("typeThree").innerHTML = Sedan.getTypeThree();
+document.getElementById("occupancyThree").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancyThree();
+document.getElementById("colorThree").innerHTML = 'Color Available:' + ' ' + Sedan.getColorThree();
+document.getElementById("manufacturedThree").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
+
+document.getElementById("typeFour").innerHTML = Sedan.getTypeFour();
+document.getElementById("occupancyFour").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancyFour();
+document.getElementById("colorFour").innerHTML = 'Color Available:' + ' ' + Sedan.getColorFour();
+document.getElementById("manufacturedFour").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
+
+document.getElementById("typeFive").innerHTML = Sedan.getTypeFive();
+document.getElementById("occupancyFive").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancyFive();
+document.getElementById("colorFive").innerHTML = 'Color Available:' + ' ' + Sedan.getColorFive();
+document.getElementById("manufacturedFive").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
+
+document.getElementById("typeSix").innerHTML = Sedan.getTypeSix();
+document.getElementById("occupancySix").innerHTML = 'Seats:' + ' ' + Sedan.getOccupancySix();
+document.getElementById("colorSix").innerHTML = 'Color Available:' + ' ' + Sedan.getColorSix();
+document.getElementById("manufacturedSix").innerHTML = 'Manufactoring Date:' + ' ' + Sedan.manufactured_Date;
